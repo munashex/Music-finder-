5dd90f9582849afe1aa6a0d4055aea9c52f9fd81
munashe <munashemugondaa@gmail.com>
2022-11-02 15:00:29 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,91 @@
+import React, {useEffect} from "react"
+import { Container, InputGroup, FormControl, Button, Row, Col, Card } from "react-bootstrap"
+
+
+
+const App = () => {
+
+  const CLIENT_ID = "d40282a24a3c4fe09b7d47568c04c651" 
+  const CLIENT_SECRET = "6046124f71ae4205891b00ffc8919c09"  
+
+  const [accessToken, setAccessToken] = React.useState('')
+  const [searchInput, setSearchInput] = React.useState('')
+  const [album, setAlbum] = React.useState([])
+
+ useEffect(() => {
+  var authParameters = {
+    method: "POST", 
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }, 
+    body: 'grant_type=client_credentials&client_id='+ CLIENT_ID + '&client_secret='+ CLIENT_SECRET
+  }
+ 
+  fetch('https://accounts.spotify.com/api/token', authParameters) 
+  .then((result) => result.json())
+  .then(data => setAccessToken(data.access_token))
+ }, [])
+
+
+
+ const Search = async() =>  {
+
+  var artistParameters = {
+    method: "GET", 
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': "Bearer "+ accessToken
+    }
+  }
+ 
+
+  var artistId = await fetch('https://api.spotify.com/v1/search?q='+ searchInput + '&type=artist'  , artistParameters)
+  .then((res) => res.json()) 
+  .then((response) => {return response.artists.items[0].id})  
+
+  var returnedAlbum = await fetch('https://api.spotify.com/v1/artists/'+ artistId + '/albums' + '?includes_groups=album&market=US&limit=50', artistParameters)
+  .then((res) => res.json())  
+  .then(res => setAlbum(res.items))
+ }
+ 
+
+
+  return (
+    <Container>
+    <InputGroup className="mt-3" size='lg'>
+    <FormControl 
+    placeholder="search artist" 
+    onKeyPress={(e) => {
+      if(e.key === "Enter") {
+       Search()
+      }
+    }} 
+    onChange={(e) => setSearchInput(e.target.value)} 
+    />
+    <Button onClick={Search}>Search</Button>
+    </InputGroup> 
+   
+   <br/> 
+   <br/>
+    <Container> 
+      <Row className="mx-2 row row-cols-2">
+         {
+          album.map((item, i) => {
+            return (
+              <Card>
+                 <Card.Img src={item.images[0].url}/> 
+                 <Card.Body>
+                  <Card.Title>{item.name.slice(0, 17)}..</Card.Title>
+                 </Card.Body>
+              </Card>
+    
+            )
+          })
+         }
+      </Row>
+    </Container>
+    </Container>
+  )
+}
+
+export default App
